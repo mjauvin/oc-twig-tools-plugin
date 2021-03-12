47f8c894b39b7f7a0b7392953f4de497cde47772
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-12 15:20:51 -0500

    make sure functions are not defined twice

--- init.php
+++ init.php
@@ -1,16 +1,20 @@
 <?php
 
-function lipsum($n = 1, $type = "s", $seperator = null)
-{
-    $result = '';
-    $gen = new \Badcow\LoremIpsum\Generator();
-    if ($type === 's') {
-        $result = ($seperator ?: '<p>') . implode($seperator ?: '<p>', $gen->getSentences($n));
-    } else if ($type === 'w') {
-        $result = implode($seperator ?: ' ', $gen->getRandomWords($n));
-    } else if ($type === 'p') {
-        $result = ($seperator ?: '<p>') . implode($seperator ?: '<p>', $gen->getParagraphs($n));
-    }
 
-    return trim($result);
+if (!function_exists('lipsum')) {
+
+    function lipsum($n = 1, $type = "s", $seperator = null)
+    {
+        $result = '';
+        $gen = new \Badcow\LoremIpsum\Generator();
+        if ($type === 's') {
+            $result = ($seperator ?: '<p>') . implode($seperator ?: '<p>', $gen->getSentences($n));
+        } else if ($type === 'w') {
+            $result = implode($seperator ?: ' ', $gen->getRandomWords($n));
+        } else if ($type === 'p') {
+            $result = ($seperator ?: '<p>') . implode($seperator ?: '<p>', $gen->getParagraphs($n));
+        }
+
+        return trim($result);
+    }
 }
